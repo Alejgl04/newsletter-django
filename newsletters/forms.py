dd2da8cc8a513313ab4df2a958b67f5a38a2fcf5
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2022-11-22 20:13:02 -0500

    forms newsletter

---
+++ newsletters/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import NewsLetterUser, NewsLetter
+
+class NewsLetterUserSignUpForm( forms.ModelForm ):
+  class Meta:
+    model  = NewsLetterUser
+    fields = ['email']
+
+class NewsLetterCreationForm( forms.ModelForm ):
+  class Meta:
+    model = NewsLetter
+    fields = ['name','subject','body','email']
