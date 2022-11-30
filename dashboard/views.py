eb8aa832828ed4bf364d5ceaff77ce451b382e58
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2022-11-29 20:51:46 -0500

    table status

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from newsletters.models import NewsLetter
+
 # Create your views here.
 class DashboardHomeView(TemplateView):
-    template_name = "dashboard/index.html"
+  template_name = "dashboard/index.html"
 
 
-class NewsLetterDashboardHomeView( TemplateView ):
-  template_name = "dashboard/list.html"
+class NewsLetterDashboardHomeView( View ):
+  def get(self, request, *args, **kwargs):
+    newsletters = NewsLetter.objects.all()
+
+    context = {
+      'newsletters': newsletters 
+    }
+
+    return render(request, 'dashboard/list.html', context) 
--- newsletters/models.py
+++ newsletters/models.py
@@ -9,11 +9,21 @@ class NewsLetterUser( models.Model ):
     return self.email
 
 class NewsLetter( models.Model ):
+
+  EMAILS_STATUS_CHOICES = (
+    ('Draft,','Draft'),
+    ('Published,','Published')
+  )
+
   name    = models.CharField( max_length=250 )
   subject = models.CharField( max_length=250 )
   body    = models.TextField( blank=True, null=True )
   email   = models.ManyToManyField( NewsLetterUser )
   created = models.DateTimeField( auto_now_add=True )
+  status  = models.CharField( max_length= 10 , choices= EMAILS_STATUS_CHOICES )
 
   def __str__(self):
     return self.name
+  
+  class Meta:
+    ordering = ('-created',)
